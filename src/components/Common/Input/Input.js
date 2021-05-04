b3dcb884edcf3f9c4b03fdfb8b4590589e83daea
Xentras <andreastynell@gmail.com>
2021-05-04 17:32:43 +0200

    Remove package-lock.json and small fix

--- src/components/Common/Input/Input.js
+++ src/components/Common/Input/Input.js
@@ -5,8 +5,10 @@ import { connect } from "react-redux";
 function Input(props) {
   const model = props.model;
   const placeholder = props.placeholder;
-  
-  return <Control.input model={model} placeholder={placeholder} defaultValue="" />;
+
+  return (
+    <Control.input model={model} placeholder={placeholder} defaultValue="" />
+  );
 }
 
 export default connect((s) => s)(Input);
